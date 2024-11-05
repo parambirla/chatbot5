6b0c68c9254339ca1e79befe0b4e49d02d3d4a4c
parambirla <104878929+parambirla@users.noreply.github.com>
2024-11-06 01:54:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ def api():
 
     global conversation_history
 
-    #personality = Extraversion
-    message = request.json.get("message") + ' . (Respond to this assuming "Extraversion" personality trait without revealing "Extraversion" statement in response and give response of minimum 120 words) '
+    #personality = Neuroticism
+    message = request.json.get("message") + ' . (Respond to this assuming "Neuroticism" personality trait without revealing "Neuroticism" statement in response and give response for minimum 200 words) '
 
     conversation_history.append({"role": "user", "content": message})
     # Send the message to OpenAI's API and receive the response
